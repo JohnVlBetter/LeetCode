65a93b6e5b545c732667f2b490e42904b2cd9d6b
JohnLee <lijiang01001@163.com>
2023-04-05 10:33:24 +0800

    [263] 丑数

---
+++ 263.丑数.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=cpp
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isUgly(int n) {
+        for(int i=2;i<6&&n>0;++i){
+            while(n % i == 0) n /= i;
+        }
+        return n==1;
+    }
+};
+// @lc code=end
+
