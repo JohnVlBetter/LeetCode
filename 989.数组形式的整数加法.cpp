beec1686eedd301b99720fa74858084b54c659c9
JohnLee <lijiang01001@163.com>
2023-04-11 22:20:27 +0800

    [989] 数组形式的整数加法

--- 989.数组形式的整数加法.cpp
+++ 989.数组形式的整数加法.cpp
@@ -8,7 +8,23 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-
+        vector<int> result;
+        int n = num.size();
+        for (int i = n - 1; i >= 0; --i) {
+            int sum = num[i] + k % 10;
+            k /= 10;
+            if (sum >= 10) {
+                k++;
+                sum -= 10;
+            }
+            result.push_back(sum);
+        }
+        while(k > 0) {
+            result.push_back(k % 10);
+            k /= 10;
+        }
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
 // @lc code=end
