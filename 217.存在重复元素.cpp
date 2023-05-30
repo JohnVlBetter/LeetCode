bc0dfb5aa3d25fe38872bdbcf3a533b80d831971
JohnLee <lijiang01001@163.com>
2023-05-30 22:17:37 +0800

    [217] 存在重复元素

---
+++ 217.存在重复元素.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=cpp
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int x: nums) {
+            if (s.find(x) != s.end()) {
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
+    }
+};
+// @lc code=end
+
