62c27de8a817c01cd7d90c08794268ad8e375ab3
JohnLee <lijiang01001@163.com>
2023-07-04 23:02:43 +0800

    [62] 不同路径

---
+++ 62.不同路径.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=cpp
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> map(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) map[i][0] = 1;
+        for (int j = 0; j < n; ++j) map[0][j] = 1;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                map[i][j] = map[i - 1][j] + map[i][j - 1];
+            }
+        }
+        return map[m - 1][n - 1];
+    }
+};
+// @lc code=end
+
