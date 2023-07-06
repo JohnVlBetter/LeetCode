9ff32a0809214f7fafb73e0654ac19c6e53c715f
JohnLee <lijiang01001@163.com>
2023-07-06 23:35:08 +0800

    [67] 二进制求和

--- 67.二进制求和.cpp
+++ 67.二进制求和.cpp
@@ -3,21 +3,27 @@
  *
  * [67] 二进制求和
  */
-#include <string>
-using namespace std;
-// @lc code=start
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i=a.length(),j=b.length();
-        bool flag = false;
-        for(;i>=0&&j>=0;--i,--j){
-            if(a[i] == '0'){
-                if(a[i] == b[i]){
-                    
-                }
-            }
+        int bl = b.length(), al = a.length();
+        while(bl < al){
+            b = '0' + b;
+            ++bl;
         }
+        while(al < bl){
+            a = '0' + a;
+            ++al;
+        }
+        int num = 0;
+        for(int i = al - 1;i>=0;--i){
+            int sum = a[i] - '0' + b[i] - '0' + num;
+            a[i] = (sum) % 2+'0';
+			num = sum / 2;
+        }
+        if (num > 0)//有溢出
+			a = '1' + a;
+		return a;
     }
 };
 // @lc code=end
