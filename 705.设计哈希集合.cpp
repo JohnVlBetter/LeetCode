e3cf07ddf502118c954a9386f4113fb6a49a999a
JohnLee <lijiang01001@163.com>
2023-08-05 09:46:22 +0800

    [705] 设计哈希集合

---
+++ 705.设计哈希集合.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=705 lang=cpp
+ *
+ * [705] 设计哈希集合
+ */
+
+// @lc code=start
+class MyHashSet {
+private:
+    vector<list<int>> data;
+    static const int base = 769;
+    static int HashCode(int key) {
+        return key % base;
+    }
+public:
+    MyHashSet(): data(base) {
+        
+    }
+    
+    void add(int key) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return;
+            }
+        }
+        data[h].push_back(key);
+    }
+    
+    void remove(int key) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                data[h].erase(it);
+                return;
+            }
+        }
+    }
+    
+    bool contains(int key) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it) == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+// @lc code=end
+
