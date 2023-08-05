ff484292e7404451f467563ec00e39ab60b3776f
JohnLee <lijiang01001@163.com>
2023-08-05 10:06:14 +0800

    [706] 设计哈希映射

---
+++ 706.设计哈希映射.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=706 lang=cpp
+ *
+ * [706] 设计哈希映射
+ */
+
+// @lc code=start
+class MyHashMap {
+private:
+    vector<list<pair<int, int>>> data;
+    static int HashCode(int key) {
+        return key % 857;
+    }
+public:
+    MyHashMap(): data(857) {}
+    
+    void put(int key, int value) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                (*it).second = value;
+                return;
+            }
+        }
+        data[h].push_back(make_pair(key, value));
+    }
+    
+    int get(int key) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                return (*it).second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        int h = HashCode(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                data[h].erase(it);
+                return;
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+// @lc code=end
+
