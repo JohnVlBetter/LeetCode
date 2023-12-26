6c5e8cb4c385725794b8eb6c293253eea0f37812
JohnLee <lijiang01001@163.com>
2023-12-26 20:41:30 +0800

    [2396] 严格回文的数字

---
+++ 2396. 严格回文的数字.cpp
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2974 lang=rust
+ *
+ * [2396] 严格回文的数字
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn is_strictly_palindromic(n: i32) -> bool {
+        false
+    }
+}
+// @lc code=end
+
