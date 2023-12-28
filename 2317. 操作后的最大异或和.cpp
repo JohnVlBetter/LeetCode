4259816e7803e550affcdd870a47dc229204b793
JohnLee <lijiang01001@163.com>
2023-12-28 22:19:18 +0800

    [2317] 操作后的最大异或和

---
+++ 2317. 操作后的最大异或和.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2317 lang=rust
+ *
+ * [2317] 操作后的最大异或和
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn maximum_xor(nums: Vec<i32>) -> i32 {
+        let mut res = 0;
+        for num in nums{
+            res |= num
+        }
+        res
+    }
+}
+// @lc code=end
+
