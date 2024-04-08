5818e6781d5e1699db7e9bf90808613dc6bdb258
johnkunlunpc\John <lijiang01001@163.com>
2024-04-08 12:44:55 +0800

    [2309] 兼具大小写的最好英文字母

---
+++ 2309. 兼具大小写的最好英文字母.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=2309 lang=cpp
+ *
+ * [2309] 兼具大小写的最好英文字母
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string greatestLetter(string s) {
+        int lower = 0, upper = 0;
+        for (auto ch : s) {
+            if (islower(ch)) {
+                lower |= 1 << (ch - 'a');
+            } else {
+                upper |= 1 << (ch - 'A');
+            }
+        }
+        for (int i = 25; i >= 0; i--) {
+            if (lower & upper & (1 << i)) {
+                return string(1, 'A' + i);
+            }
+        }
+        return "";
+    }
+};
+// @lc code=end
+
